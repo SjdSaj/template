9af664cc68fe58ec7e1f96548bcb7fb0ca82224c
sajad <sjdsaj63@gmail.com>
2022-09-11 14:19:41 +0530

    template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  {
   title = 'Template-Form';
+
+  user = { email : '' , pswd : ''};
+
+  submit(data:any){
+    alert('working');
+    console.log(data.form.status);
+  }
 }
